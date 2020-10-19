53a69c3b11c58f6ddfc661a48707094ad5097101
gfmasca <gfmascarenhas@gmail.com>
2020-10-19 01:44:09 -0300

    Add InfoPiu interface

---
+++ src/interfaces/InfoPiu.d.ts
@@ -0,0 +1,13 @@
+import User from './User';
+
+interface InfoPiu {
+    id: number;
+    usuario: User;
+    // likers: Array<User>;
+    likers: Array<number>;
+    // favoritado_por: Array<User>;
+    texto: string;
+    // horario: string;
+};
+
+export default InfoPiu;
