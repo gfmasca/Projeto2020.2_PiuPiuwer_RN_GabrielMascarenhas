ec012eb9ced98bbee35f4fc0d3214bb7bf58d2db
gfmasca <gfmascarenhas@gmail.com>
2020-10-19 01:43:14 -0300

    Implement Feed css

--- src/pages/Feed/index.tsx
+++ src/pages/Feed/index.tsx
@@ -1,13 +1,43 @@
-import React from "react";
+import React, { useCallback } from "react";
 import { Text, View } from "react-native";
+import { RectButton } from "react-native-gesture-handler";
 import { SafeAreaView } from "react-native-safe-area-context";
+import Header from "../../components/Header";
+import { useAuth } from "../../hooks/useAuth";
 
-import {} from './styles'; 
+import BirdImage from '../../assets/images/bird-singing.png';
+
+import {
+    ContentContainer,
+    NewPiuContainer,
+    NewPiuText,
+    NewPiuButton,
+    NewPiuImage,
+    PiusContainer
+} from './styles'; 
+import Piu from "../../components/Piu";
 
 const Feed: React.FC = () => {
+    const { logout, user } = useAuth()
+
+    const handleLogout = useCallback(() => {
+        logout();
+    }, []);
+
     return (
         <SafeAreaView>
-            <Text>eae meu povo</Text>
+            <Header />
+            {/* <RectButton onPress={ handleLogout } ><Text>Logout</Text></RectButton> */}
+            <ContentContainer>
+                <NewPiuContainer>
+                    <NewPiuText>Dê um novo Piu</NewPiuText>
+                    <NewPiuButton><NewPiuImage source={ BirdImage } /></NewPiuButton>
+                </NewPiuContainer>
+
+                <PiusContainer>
+                    <Piu id={1} texto='eaemeupovo' likers={[1, 2, 3]} usuario={user} />
+                </PiusContainer>
+            </ContentContainer>
         </SafeAreaView>
     )
 }
--- src/pages/Feed/styles.ts
+++ src/pages/Feed/styles.ts
@@ -1,2 +1,41 @@
-import styled from "styled-components";
+import styled from "styled-components/native";
+import { RectButton } from 'react-native-gesture-handler';
 
+export const ContentContainer = styled.View`
+    height: 94%;
+    width: 100%;
+`
+
+export const NewPiuContainer = styled.View`
+    height: 10%;
+    width: 100%;
+    background-color: #212529;
+    flex-direction: row;
+    justify-content: space-between;
+    padding: 20px 20px 20px 20px;
+    align-items: center;
+`
+
+export const NewPiuText = styled.Text`
+    color: #F8F9FA;
+    font-size: 16px;
+    letter-spacing: 1.5px;
+`
+
+export const NewPiuButton = styled(RectButton)`
+    border-radius: 30px;
+    width: 60px;
+    height: 60px;
+    background-color: #E9ECEF;
+    align-items: center;
+    justify-content: center;
+`
+
+export const NewPiuImage = styled.Image`
+`
+
+export const PiusContainer = styled.View`
+    height: 90%;
+    width: 100%;
+    background-color: #343A40;
+`
